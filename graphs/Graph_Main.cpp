16b5ed82aaa99d66a8bac4c60d40faf40db32a18
Fluffy-katy <154894474+Fluffy-katy@users.noreply.github.com>
2024-11-04 05:49:30 -0800

    Runnig the first algorithm

--- graphs/Graph_Main.cpp
+++ graphs/Graph_Main.cpp
@@ -1,10 +1,63 @@
 // Capitan's file
 #include <iostream>
+#include <chrono>
+#include <algorithm>
+#include <string>
+#include <chrono>
+
+#include "Graph.h"
+
 using namespace std;
+
+#define RUN(x, show_sort) {                \
+    Run(#x, x, data, show_sort);           \
+}
+
+
+//template<typename FindWayMethod>
+//void Run(FindWayMethod findway_method) {
+//    cout << "Running the algorithm: " << method_name << endl;
+//
+//    auto start = chrono::high_resolution_clock::now();
+//    
+//    findway_method_method();
+//    auto stop = chrono::system_clock::now();
+//    auto time = chrono::duration_cast<chrono::microseconds>(stop - start).count();
+//
+//    cout << "Algorithm " << method_name << (is_sorted ? " completed successfully." : " completed with an error.") << endl;
+//    cout << "Time: " << time << " mcs" << endl << endl;
+//
+//}
+
 
 int main() {
 
-    cout << "Hello, world!" << endl;
+    Connections connections;
+
+    ifstream fin;
+    fin.open("Graph/graph1.txt");
+    int count_connections;
+    fin >> count_connections;
+
+    while (count_connections--) {
+        Edge edge;
+        fin >> edge;
+        connections.insert(edge);
+    }
+    fin.close();
+
+
+
+    // Watch Example (how
+
+    Graph graph = Graph(connections.edges, connections.nodes);
+    vector<int> result = graph.metodDeikstra(1, 6);
+
+    for (auto node : result) {
+        cout << node << " -> ";
+    }
+    cout << "end" << endl;
+    cout << "Len: " << graph.lenghtShortestPath(result) << endl;
     return 0;
 
 }
