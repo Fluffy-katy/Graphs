8ff83c04525c08a90eaabf813179fc3e61349c56
mariroiss <elenasimf4@gmail.com>
2024-11-20 13:53:02 +0300

    updated DFS

--- graphs/Algorithm.h
+++ graphs/Algorithm.h
@@ -5,3 +5,6 @@
 std::vector<int> FloydWarshall(std::vector<std::vector<double>> adjMatrix, int start, int end);
 
 std::vector<int> BFS(std::vector<std::vector<double>> adj_matrix, int sourse, int sink);
+
+bool DFS_Recursive(std::vector<std::vector<double>> adjMatrix,
+    int start, int end, std::vector<bool>& visited, std::vector<int>& path);
--- graphs/DFS.cpp
+++ graphs/DFS.cpp
@@ -4,9 +4,10 @@
 using namespace std;
 
 // Функция DFS для поиска пути в графе
-bool Graph::DFS_Recursive(vector<vector<double>> adjMatrix, int start, int end, vector<bool>& visited, vector<int>& path) {
+bool DFS_Recursive(vector<vector<double>> adjMatrix, int start, int end, vector<bool>& visited, vector<int>& path) {
     visited[start] = true;
-    path.push_back(nodes.at(start));
+    /*path.push_back(nodes.at(start));*/
+    path.push_back(start);
 
     if (start == end) {
         return true;
@@ -14,7 +15,7 @@ bool Graph::DFS_Recursive(vector<vector<double>> adjMatrix, int start, int end, 
 
     // Проходим по всем соседям
     for (int i = 0; i < adjMatrix.size(); i++) {
-        if (adjMatrix[start][i] != 0 && !visited[i]) { 
+        if (adjMatrix[start][i] > 0 && !visited[i]) {
             if (DFS_Recursive(adjMatrix, i, end, visited, path)) {
                 return true;
             }
@@ -25,11 +26,11 @@ bool Graph::DFS_Recursive(vector<vector<double>> adjMatrix, int start, int end, 
 }
 
 // Основная функция для вызова DFS
-vector<int> Graph::DFS(vector<vector<double>> adjMatrix, int start, int end) {
+vector<int> DFS(vector<vector<double>> adjMatrix, int start, int end) {
     vector<bool> visited(adjMatrix.size(), false);
     vector<int> path;
-    start = GetIndex(start);
-    end = GetIndex(end);
+    //start = GetIndex(start);
+    //end = GetIndex(end);
 
     if (DFS_Recursive(adjMatrix, start, end, visited, path)) {
         return path;
